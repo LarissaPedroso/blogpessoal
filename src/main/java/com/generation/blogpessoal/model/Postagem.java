f8675e41839cd119c8f438e6d28a747b42ef7a25
LarissaPedroso <lary_santos2000@hotmail.com>
2024-01-22 16:47:21 -0300

    Security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -38,9 +38,13 @@ public class Postagem {
 	private LocalDateTime data;
 	
 	@ManyToOne
-	@JsonIgnoreProperties("Postagem")
+	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
 	public Long getId() {
 		return id;
 	}
@@ -81,6 +85,12 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
 	
+	public void setUsuario(Usuario usuario) {
+		this.usuario =  usuario;
+	}
 	
 }
